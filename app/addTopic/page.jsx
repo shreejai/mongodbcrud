d528cd18e71c0f4c6c5b4a0f692a81d4de675aca
shreejai <shreejai@icloud.com>
2023-08-31 23:01:44 +1000

    topiclist and add topic component

---
+++ app/addTopic/page.jsx
@@ -0,0 +1,19 @@
+export default function AddTopic () {
+  return (
+    <form className="flex flex-col gap-3">
+      <input 
+        className="border border-slate-500 px-8 py-2" 
+        type="text" 
+        placeholder="Topic Title"          
+      />
+
+      <input 
+        className="border border-slate-500 px-8 py-2" 
+        type="text" 
+        placeholder="Topic Description"          
+      />
+
+      <button className="bg-green-600 font-bold text-white py-3 w-fit px-6">Add Topic</button>
+    </form>
+  );
+}
---
+++ app/editTopic/[id]/page.jsx
@@ -0,0 +1,5 @@
+export default function EditTopic() {
+  return (
+    <div>edit topic</div>
+  );
+}
